0715bd1c33ae80144d36601c7639bdef3d968900
Thinhhuhydss <thinhuynh115@gmail.com>
2023-01-08 04:08:09 +0700

    screen playMusic

---
+++ src/screen/profileMusic.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function ProfileMusic() {
+  return (
+    <View>
+      <Text>profileMusic</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
