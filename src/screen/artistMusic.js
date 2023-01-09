74961762c619bb7d084181a1e839e463fbf29b48
Trần Anh Quân <anhquan28092001@gmail.com>
2023-01-09 16:25:32 +0700

    ui list artist

---
+++ src/screen/artistMusic.js
@@ -0,0 +1,132 @@
+import React from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import ConversationItem from '../component/ConversationItem'
+
+const conversations = [
+    {
+        id: 1,
+        name: "Erik",
+        type: "Nghệ sĩ",
+        avatar: 'https://znews-photo.zingcdn.me/w1920/Uploaded/wobvjuz/2022_02_18/erik_dau_nhat_la_lang_im_04.jpg',
+    },
+    {
+        id: 2,
+        name: "Sơn Tùng MTP",
+        type: "Nghệ sĩ",
+        avatar: 'https://ss-images.saostar.vn/wp700/pc/1655895094264/saostar-zemx7kpv9n1wnysy.jpg',
+    },
+    {
+        id: 3,
+        name: "Erik",
+        type: "Nghệ sĩ",
+        avatar: 'https://znews-photo.zingcdn.me/w1920/Uploaded/wobvjuz/2022_02_18/erik_dau_nhat_la_lang_im_04.jpg',
+    },
+    {
+        id: 4,
+        name: "Sơn Tùng MTP",
+        type: "Nghệ sĩ",
+        avatar: 'https://ss-images.saostar.vn/wp700/pc/1655895094264/saostar-zemx7kpv9n1wnysy.jpg',
+    },
+    {
+        id: 5,
+        name: "Erik",
+        type: "Nghệ sĩ",
+        avatar: 'https://znews-photo.zingcdn.me/w1920/Uploaded/wobvjuz/2022_02_18/erik_dau_nhat_la_lang_im_04.jpg',
+    },
+    {
+        id: 6,
+        name: "Sơn Tùng MTP",
+        type: "Nghệ sĩ",
+        avatar: 'https://ss-images.saostar.vn/wp700/pc/1655895094264/saostar-zemx7kpv9n1wnysy.jpg',
+    },
+    {
+        id: 7,
+        name: "Erik",
+        type: "Nghệ sĩ",
+        avatar: 'https://znews-photo.zingcdn.me/w1920/Uploaded/wobvjuz/2022_02_18/erik_dau_nhat_la_lang_im_04.jpg',
+    },
+    {
+        id: 8,
+        name: "Sơn Tùng MTP",
+        type: "Nghệ sĩ",
+        avatar: 'https://ss-images.saostar.vn/wp700/pc/1655895094264/saostar-zemx7kpv9n1wnysy.jpg',
+    },
+    {
+        id: 7,
+        name: "Erik",
+        type: "Nghệ sĩ",
+        avatar: 'https://znews-photo.zingcdn.me/w1920/Uploaded/wobvjuz/2022_02_18/erik_dau_nhat_la_lang_im_04.jpg',
+    },
+    {
+        id: 8,
+        name: "Sơn Tùng MTP",
+        type: "Nghệ sĩ",
+        avatar: 'https://ss-images.saostar.vn/wp700/pc/1655895094264/saostar-zemx7kpv9n1wnysy.jpg',
+    },
+    {
+        id: 7,
+        name: "Erik",
+        type: "Nghệ sĩ",
+        avatar: 'https://znews-photo.zingcdn.me/w1920/Uploaded/wobvjuz/2022_02_18/erik_dau_nhat_la_lang_im_04.jpg',
+    },
+    {
+        id: 8,
+        name: "Sơn Tùng MTP",
+        type: "Nghệ sĩ",
+        avatar: 'https://ss-images.saostar.vn/wp700/pc/1655895094264/saostar-zemx7kpv9n1wnysy.jpg',
+    },
+    {
+        id: 7,
+        name: "Erik",
+        type: "Nghệ sĩ",
+        avatar: 'https://znews-photo.zingcdn.me/w1920/Uploaded/wobvjuz/2022_02_18/erik_dau_nhat_la_lang_im_04.jpg',
+    },
+    {
+        id: 8,
+        name: "Sơn Tùng MTP",
+        type: "Nghệ sĩ",
+        avatar: 'https://ss-images.saostar.vn/wp700/pc/1655895094264/saostar-zemx7kpv9n1wnysy.jpg',
+    },
+    {
+        id: 7,
+        name: "Erik",
+        type: "Nghệ sĩ",
+        avatar: 'https://znews-photo.zingcdn.me/w1920/Uploaded/wobvjuz/2022_02_18/erik_dau_nhat_la_lang_im_04.jpg',
+    },
+    {
+        id: 8,
+        name: "Sơn Tùng MTP",
+        type: "Nghệ sĩ",
+        avatar: 'https://ss-images.saostar.vn/wp700/pc/1655895094264/saostar-zemx7kpv9n1wnysy.jpg',
+    },
+
+]
+
+export default function ArtistMusic() {
+    return (
+        <View style={styles.rootView}>
+            <View >
+                <Text style={{ fontSize: 30, fontWeight: 'bold', padding: 20 }}>Nghệ Sĩ</Text>
+
+            </View>
+            <FlatList
+                data={conversations}
+                renderItem={({ item, index }) => <ConversationItem conversation={item} />}
+                keyExtractor={(item, index) => index.toString()}
+
+
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    rootView: {
+        flex: 1
+    },
+    nameText: {
+        fontSize: 24,
+        alignSelf: 'center'
+
+    }
+})
